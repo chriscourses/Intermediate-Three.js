a019e4aefeae58e83f39586de243699b758fc91c
christopher4lis <christopher4lis@gmail.com>
2021-09-11 16:41:29 -0400

    Lesson 11 - Inserting Bulk Country Data

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import './tailwind.css'
 import gsap from 'gsap'
 import * as THREE from 'three'
+import countries from './countries.json'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
 import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+
+console.log(countries)
 
 const canvasContainer = document.querySelector('#canvasContainer')
 
@@ -122,36 +125,93 @@ function createBox({ lat, lng, country, population }) {
   box.population = population
 }
 
-createBox({
-  lat: 23.6345,
-  lng: -102.5528,
-  country: 'Mexico',
-  population: '127.6 million'
-})
-createBox({
-  lat: -14.235,
-  lng: -51.9253,
-  country: 'Brazil',
-  population: '211 million'
-})
-createBox({
-  lat: 20.5937,
-  lng: 78.9629,
-  country: 'India',
-  population: '1.366 billion'
-})
-createBox({
-  lat: 35.8617,
-  lng: 104.1954,
-  country: 'China',
-  population: '1.398 billion'
-})
-createBox({
-  lat: 37.0902,
-  lng: -95.7129,
-  country: 'USA',
-  population: '328.2 million'
-})
+function createBoxes(countries) {
+  countries.forEach((country) => {
+    const scale = country.population / 1000000000
+    const lat = country.latlng[0]
+    const lng = country.latlng[1]
+    const zScale = 0.8 * scale
+
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(
+        Math.max(0.1, 0.2 * scale),
+        Math.max(0.1, 0.2 * scale),
+        Math.max(zScale, 0.4 * Math.random())
+      ),
+      new THREE.MeshBasicMaterial({
+        color: '#3BF7FF',
+        opacity: 0.4,
+        transparent: true
+      })
+    )
+
+    // 23.6345° N, 102.5528° W = mexico
+    const latitude = (lat / 180) * Math.PI
+    const longitude = (lng / 180) * Math.PI
+    const radius = 5
+
+    const x = radius * Math.cos(latitude) * Math.sin(longitude)
+    const y = radius * Math.sin(latitude)
+    const z = radius * Math.cos(latitude) * Math.cos(longitude)
+
+    box.position.x = x
+    box.position.y = y
+    box.position.z = z
+
+    box.lookAt(0, 0, 0)
+    box.geometry.applyMatrix4(
+      new THREE.Matrix4().makeTranslation(0, 0, -zScale / 2)
+    )
+
+    group.add(box)
+
+    gsap.to(box.scale, {
+      z: 1.4,
+      duration: 2,
+      yoyo: true,
+      repeat: -1,
+      ease: 'linear',
+      delay: Math.random()
+    })
+    // box.scale.z =
+
+    box.country = country.name
+    box.population = new Intl.NumberFormat().format(country.population)
+  })
+}
+
+createBoxes(countries)
+
+// createBox({
+//   lat: 23.6345,
+//   lng: -102.5528,
+//   country: 'Mexico',
+//   population: '127.6 million'
+// })
+// createBox({
+//   lat: -14.235,
+//   lng: -51.9253,
+//   country: 'Brazil',
+//   population: '211 million'
+// })
+// createBox({
+//   lat: 20.5937,
+//   lng: 78.9629,
+//   country: 'India',
+//   population: '1.366 billion'
+// })
+// createBox({
+//   lat: 35.8617,
+//   lng: 104.1954,
+//   country: 'China',
+//   population: '1.398 billion'
+// })
+// createBox({
+//   lat: 37.0902,
+//   lng: -95.7129,
+//   country: 'USA',
+//   population: '328.2 million'
+// })
 
 sphere.rotation.y = -Math.PI / 2
 
